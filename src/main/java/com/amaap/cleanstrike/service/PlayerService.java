d620eb2a6691a8592c9efcc34058e8fc7af9b8c1
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-11 03:07:21 +0530

    Add Player Service class

---
+++ src/main/java/com/amaap/cleanstrike/service/PlayerService.java
@@ -0,0 +1,31 @@
+package com.amaap.cleanstrike.service;
+
+import com.amaap.cleanstrike.Http;
+import com.amaap.cleanstrike.controller.dto.Response;
+import com.amaap.cleanstrike.repository.PlayerRepository;
+import com.amaap.cleanstrike.repository.exception.DuplicateIdException;
+
+import java.util.Set;
+
+public class PlayerService {
+    PlayerRepository playerRepository;
+
+    public Response validateToCreate(int playerOneId, int playerTwoId) throws DuplicateIdException {
+
+        if (playerOneId <= 0 || playerTwoId <= 0) {
+            return new Response(Http.BAD_REQUEST);
+        } else {
+            createPlayer(playerOneId, playerTwoId);
+            return new Response(Http.OK);
+        }
+    }
+
+    void createPlayer(int id1, int id2) throws DuplicateIdException {
+        playerRepository.insertPlayer(id1, id2);
+    }
+
+
+    public Set<Integer> getCreatedPlayer() {
+        return playerRepository.getTotalPlayers();
+    }
+}
