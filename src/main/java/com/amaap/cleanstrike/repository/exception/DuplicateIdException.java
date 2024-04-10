7e7021fa5ded4e1db65358c147d06e0a9406bffb
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-11 03:09:04 +0530

    Add custome exception

---
+++ src/main/java/com/amaap/cleanstrike/repository/exception/DuplicateIdException.java
@@ -0,0 +1,8 @@
+package com.amaap.cleanstrike.repository.exception;
+
+public class DuplicateIdException extends  Exception {
+    public DuplicateIdException(String m) {
+        super(m);
+
+    }
+}
