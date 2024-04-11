8c1f067eda41d54dab8fe16de19542a584afa2cb
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-11 18:43:35 +0530

    modified player controller test

--- src/test/java/com/amaap/cleanstrike/controller/PlayerControllerTest.java
+++ src/test/java/com/amaap/cleanstrike/controller/PlayerControllerTest.java
@@ -2,41 +2,52 @@ package com.amaap.cleanstrike.controller;
 
 import com.amaap.cleanstrike.Http;
 import com.amaap.cleanstrike.controller.dto.Response;
+import com.amaap.cleanstrike.gameservice.PlayerService;
 import com.amaap.cleanstrike.repository.exception.DuplicateIdException;
+import com.amaap.cleanstrike.repository.imlemetation.InMemoryPlayerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerControllerTest {
+    private PlayerController playerController;
+    private PlayerService playerService;
+
+    @BeforeEach
+    void setUp() {
+        InMemoryPlayerRepository playerRepository = new InMemoryPlayerRepository();
+        playerService = new PlayerService(playerRepository);
+        playerController = new PlayerController(playerService);
+    }
 
     @Test
     void shouldBeAbleTORespondWithOkWhenPlayerISCreatedWithId() throws DuplicateIdException {
         // arrange
-        int playerOneId=1;
-        int playerTwoId=2;
-        PlayerController playerController=new PlayerController();
+        int playerOneId = 1;
+        int playerTwoId = 2;
 
-        Response expected=new Response(Http.OK);
+        Response expected = new Response(Http.OK);
+
         // act
-        Response actual= playerController.createPlayers(playerOneId,playerTwoId);
+        Response actual = playerController.createPlayers(playerOneId, playerTwoId);
 
         // assert
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldBeAbleTORespondWithBadRequestWhenPlayerISCreatedWithInvalidId() throws DuplicateIdException {
         // arrange
-        int playerOneId=-1;
-        int playerTwoId=2;
-        PlayerController playerController=new PlayerController();
+        int playerOneId = -1;
+        int playerTwoId = 2;
 
-        Response expected=new Response(Http.BAD_REQUEST);
+        Response expected = new Response(Http.BAD_REQUEST);
+
         // act
-        Response actual= playerController.createPlayers(playerOneId,playerTwoId);
+        Response actual = playerController.createPlayers(playerOneId, playerTwoId);
 
         // assert
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
-
 }
