c38bb5aa156b9e362dd435b2b3240da5663e982d
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-11 18:45:23 +0530

    modified player repository

--- src/main/java/com/amaap/cleanstrike/repository/PlayerRepository.java
+++ src/main/java/com/amaap/cleanstrike/repository/PlayerRepository.java
@@ -2,23 +2,12 @@ package com.amaap.cleanstrike.repository;
 
 import com.amaap.cleanstrike.repository.exception.DuplicateIdException;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-
-public class PlayerRepository {
-
-    private Set<Integer> totalPlayers = new HashSet<>();
-
-    public void insertPlayer(int playerOneId, int playerTwoId) throws DuplicateIdException {
-        if (totalPlayers.contains(playerOneId) || totalPlayers.contains(playerTwoId)) {
-            throw new DuplicateIdException("Duplicate player ID found.");
-        }
-        totalPlayers.add(playerOneId);
-        totalPlayers.add(playerTwoId);
+    public interface PlayerRepository {
+        void insertPlayer(int playerOneId, int playerTwoId) throws DuplicateIdException;
+        Set<Integer> getTotalPlayers();
     }
 
-    public Set<Integer> getTotalPlayers() {
-        return totalPlayers;
-    }
-}
