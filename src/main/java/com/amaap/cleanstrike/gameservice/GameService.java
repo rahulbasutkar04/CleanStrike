41122850939851cfb031c128b40dc73ecf15665d
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-04-12 06:45:02 +0530

    Update gameservice class

--- src/main/java/com/amaap/cleanstrike/gameservice/GameService.java
+++ src/main/java/com/amaap/cleanstrike/gameservice/GameService.java
@@ -10,14 +10,24 @@ import java.util.List;
 import java.util.Set;
 
 public class GameService {
-       PlayerRepository playerRepository;
-       PlayerService playerService=new PlayerService(playerRepository);
-       Set<Integer> playerIds=new HashSet<>();
+    private final PlayerService playerService;
+    private final Set<Integer> playerIds = new HashSet<>();
+
+    // Constructor
+    public GameService(PlayerRepository playerRepository) {
+        this.playerService = new PlayerService(playerRepository);
+    }
+
     public Response getPlayers(PlayerService playerService) {
-
-       playerIds= playerService.getCreatedPlayer();
-
+        playerIds.addAll(playerService.getCreatedPlayer());
+        if(playerIds.isEmpty()) {
+            return new Response(Http.BAD_REQUEST);
+        }
         return new Response(Http.OK);
+    }
+
+    public Set<Integer> getCreatedPlayer() {
+        return playerIds;
     }
 }
 
